650df003aafab270e7de7788a47e00879624c929
Muhammad Reza Ramadhan <rezaramadhan.m@gmail.com>
2016-10-24 14:03:58 +0700

    Add timer

--- PemiraServer/PemiraServer/MainForm.Designer.cs
+++ PemiraServer/PemiraServer/MainForm.Designer.cs
@@ -39,12 +39,12 @@
             this.label3 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.labelTimerBilik1 = new System.Windows.Forms.Label();
+            this.labelBilikNIM2 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
             this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
+            this.labelTimerBilik2 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
             this.panel3 = new System.Windows.Forms.Panel();
@@ -72,7 +72,7 @@
             this.textBoxNIM.Name = "textBoxNIM";
             this.textBoxNIM.Size = new System.Drawing.Size(296, 38);
             this.textBoxNIM.TabIndex = 1;
-            this.textBoxNIM.KeyDown += new System.Windows.Forms.KeyEventHandler(enter_pressed);
+            this.textBoxNIM.KeyDown += new System.Windows.Forms.KeyEventHandler(this.enter_pressed);
             // 
             // buttonSubmitNIM
             // 
@@ -103,6 +103,7 @@
             this.buttonGrant1.TabIndex = 4;
             this.buttonGrant1.Text = "Grant Access";
             this.buttonGrant1.UseVisualStyleBackColor = true;
+            this.buttonGrant1.Click += new System.EventHandler(this.buttonGrant1_Click);
             // 
             // buttonGrant2
             // 
@@ -112,6 +113,7 @@
             this.buttonGrant2.TabIndex = 5;
             this.buttonGrant2.Text = "Grant Access";
             this.buttonGrant2.UseVisualStyleBackColor = true;
+            this.buttonGrant2.Click += new System.EventHandler(this.buttonGrant2_Click);
             // 
             // label1
             // 
@@ -147,8 +149,8 @@
             // 
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel1.Controls.Add(this.label6);
-            this.panel1.Controls.Add(this.label7);
-            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.labelTimerBilik1);
+            this.panel1.Controls.Add(this.labelBilikNIM2);
             this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.buttonGrant1);
@@ -167,25 +169,25 @@
             this.label6.TabIndex = 10;
             this.label6.Text = "time 2";
             // 
-            // label7
+            // labelTimerBilik1
             // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(152, 91);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(52, 20);
-            this.label7.TabIndex = 9;
-            this.label7.Text = "time 1";
+            this.labelTimerBilik1.AutoSize = true;
+            this.labelTimerBilik1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTimerBilik1.Location = new System.Drawing.Point(152, 91);
+            this.labelTimerBilik1.Name = "labelTimerBilik1";
+            this.labelTimerBilik1.Size = new System.Drawing.Size(52, 20);
+            this.labelTimerBilik1.TabIndex = 9;
+            this.labelTimerBilik1.Text = "time 1";
             // 
-            // label5
+            // labelBilikNIM2
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(25, 123);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(51, 20);
-            this.label5.TabIndex = 8;
-            this.label5.Text = "NIM 2";
+            this.labelBilikNIM2.AutoSize = true;
+            this.labelBilikNIM2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBilikNIM2.Location = new System.Drawing.Point(25, 123);
+            this.labelBilikNIM2.Name = "labelBilikNIM2";
+            this.labelBilikNIM2.Size = new System.Drawing.Size(51, 20);
+            this.labelBilikNIM2.TabIndex = 8;
+            this.labelBilikNIM2.Text = "NIM 2";
             // 
             // label4
             // 
@@ -202,7 +204,7 @@
             this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel2.Controls.Add(this.label8);
             this.panel2.Controls.Add(this.label2);
-            this.panel2.Controls.Add(this.label9);
+            this.panel2.Controls.Add(this.labelTimerBilik2);
             this.panel2.Controls.Add(this.label10);
             this.panel2.Controls.Add(this.buttonGrant2);
             this.panel2.Controls.Add(this.label11);
@@ -221,15 +223,15 @@
             this.label8.TabIndex = 14;
             this.label8.Text = "time 2";
             // 
-            // label9
+            // labelTimerBilik2
             // 
-            this.label9.AutoSize = true;
-            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(149, 91);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(52, 20);
-            this.label9.TabIndex = 13;
-            this.label9.Text = "time 1";
+            this.labelTimerBilik2.AutoSize = true;
+            this.labelTimerBilik2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTimerBilik2.Location = new System.Drawing.Point(149, 91);
+            this.labelTimerBilik2.Name = "labelTimerBilik2";
+            this.labelTimerBilik2.Size = new System.Drawing.Size(52, 20);
+            this.labelTimerBilik2.TabIndex = 13;
+            this.labelTimerBilik2.Text = "time 1";
             // 
             // label10
             // 
@@ -264,13 +266,14 @@
             // listViewWaiting
             // 
             this.listViewWaiting.BackColor = System.Drawing.SystemColors.Control;
+            this.listViewWaiting.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.listViewWaiting.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listViewWaiting.Location = new System.Drawing.Point(17, 46);
             this.listViewWaiting.Name = "listViewWaiting";
             this.listViewWaiting.Size = new System.Drawing.Size(195, 189);
             this.listViewWaiting.TabIndex = 11;
             this.listViewWaiting.UseCompatibleStateImageBehavior = false;
-            this.listViewWaiting.View = System.Windows.Forms.View.List;
+            this.listViewWaiting.View = System.Windows.Forms.View.Details;
             // 
             // MainForm
             // 
@@ -313,11 +316,11 @@
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelTimerBilik1;
+        private System.Windows.Forms.Label labelBilikNIM2;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label labelTimerBilik2;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.ListView listViewWaiting;
--- PemiraServer/PemiraServer/MainForm.cs
+++ PemiraServer/PemiraServer/MainForm.cs
@@ -12,34 +12,70 @@ namespace PemiraServer
 {
     public partial class MainForm : Form
     {
+        private Timer time;
+        private Timer time1;
+        private int interval = 2000;
+        
         public MainForm()
         {
             InitializeComponent();
-            TreeNode newNode = new TreeNode("Text for new node");
+            time = new Timer();
+            time.Tick += new EventHandler(time_Tick);
+            time.Interval = interval;
+            time.Tag = "halo";
+
+            time1 = new Timer();
+            time1.Tick += new EventHandler(time_Tick);
+            time1.Interval = interval;
+            time1.Tag = "hello";
+
+            ColumnHeader header = new ColumnHeader();
+            header.Text = "";
+            header.Name = "col1";
+            header.Width = listViewWaiting.Width - 25;
+            listViewWaiting.Columns.Add(header);
+            listViewWaiting.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+
         }
 
-        private void addToWaiting(string s)
+        private void time_Tick(object sender, EventArgs e)
         {
+            Timer t = sender as Timer;
+            string s = t.Tag.ToString();
+            MessageBox.Show(s);
+        }
+
+        private void addToWaiting()
+        {
+            string s = textBoxNIM.Text;
+
             if (s!= "")
                 listViewWaiting.Items.Add(s);
+
+            textBoxNIM.Text = "";
         }
 
         private void buttonSubmitNIM_Click(object sender, EventArgs e)
         {
-            string s = textBoxNIM.Text;
-            addToWaiting(s);
-            textBoxNIM.Text = "";
+            addToWaiting();
         }
 
         private void enter_pressed(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Enter)
             {
-                string s = textBoxNIM.Text;
-                addToWaiting(s);
-                textBoxNIM.Text = "";
-
+                addToWaiting();
             }
-        } 
+        }
+
+        private void buttonGrant1_Click(object sender, EventArgs e)
+        {
+            time.Start();
+        }
+
+        private void buttonGrant2_Click(object sender, EventArgs e)
+        {
+            time1.Start();
+        }
     }
 }
