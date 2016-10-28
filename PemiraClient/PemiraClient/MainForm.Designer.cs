2acac50645c2a84f0f86c5d558e8e0f785e75c2c
kristiantokarim <kristiantokarim@gmail.com>
2016-10-28 07:55:02 +0700

    pemira client

---
+++ PemiraClient/PemiraClient/MainForm.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace PemiraClient
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelSelamatDatang = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.labelNIM = new System.Windows.Forms.Label();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelSelamatDatang
+            // 
+            this.labelSelamatDatang.BackColor = System.Drawing.Color.Transparent;
+            this.labelSelamatDatang.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelSelamatDatang.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSelamatDatang.ForeColor = System.Drawing.Color.Snow;
+            this.labelSelamatDatang.Location = new System.Drawing.Point(0, 0);
+            this.labelSelamatDatang.Name = "labelSelamatDatang";
+            this.labelSelamatDatang.Size = new System.Drawing.Size(457, 63);
+            this.labelSelamatDatang.TabIndex = 0;
+            this.labelSelamatDatang.Text = "Selamat Datang !";
+            this.labelSelamatDatang.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelSelamatDatang.UseMnemonic = false;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.labelSelamatDatang);
+            this.panel1.Location = new System.Drawing.Point(176, 29);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(461, 67);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.labelNIM);
+            this.panel2.Location = new System.Drawing.Point(176, 120);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(459, 80);
+            this.panel2.TabIndex = 2;
+            // 
+            // labelNIM
+            // 
+            this.labelNIM.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelNIM.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNIM.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.labelNIM.Location = new System.Drawing.Point(0, 0);
+            this.labelNIM.Name = "labelNIM";
+            this.labelNIM.Size = new System.Drawing.Size(455, 76);
+            this.labelNIM.TabIndex = 0;
+            this.labelNIM.Text = "Hubungi operator untuk memilih";
+            this.labelNIM.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(402, 234);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(35, 13);
+            this.labelStatus.TabIndex = 3;
+            this.labelStatus.Text = "label1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 326);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelSelamatDatang;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label labelNIM;
+        private System.Windows.Forms.Label labelStatus;
+    }
+}
+
---
+++ PemiraClient/PemiraClient/MainForm.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Diagnostics;
+using Microsoft.Win32;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+
+namespace PemiraClient
+{
+        public partial class MainForm : Form
+        {
+            /***===========================DISABLE KEY==============================***/
+            [DllImport("user32.dll")]
+            public static extern int FindWindow(string lpClassName, string lpWindowName);
+            [DllImport("user32.dll")]
+            public static extern int SendMessage(int hWnd, uint Msg, int wParam, int lParam);
+
+            [return: MarshalAs(UnmanagedType.Bool)]
+            [DllImport("user32.dll", SetLastError = true)]
+            public static extern bool PostMessage(int hWnd, uint Msg, int wParam, int lParam);
+            /* Code to Disable WinKey, Alt+Tab, Ctrl+Esc Starts Here */
+
+            // Structure contain information about low-level keyboard input event 
+            [StructLayout(LayoutKind.Sequential)]
+            private struct KBDLLHOOKSTRUCT
+            {
+                public Keys key;
+                public int scanCode;
+                public int flags;
+                public int time;
+                public IntPtr extra;
+            }
+            //System level functions to be used for hook and unhook keyboard input  
+            private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+            [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            private static extern IntPtr SetWindowsHookEx(int id, LowLevelKeyboardProc callback, IntPtr hMod, uint dwThreadId);
+            [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            private static extern bool UnhookWindowsHookEx(IntPtr hook);
+            [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            private static extern IntPtr CallNextHookEx(IntPtr hook, int nCode, IntPtr wp, IntPtr lp);
+            [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            private static extern IntPtr GetModuleHandle(string name);
+            [DllImport("user32.dll", CharSet = CharSet.Auto)]
+            private static extern short GetAsyncKeyState(Keys key);
+            //Declaring Global objects     
+            private IntPtr ptrHook;
+            private LowLevelKeyboardProc objKeyboardProcess;
+
+            private IntPtr captureKey(int nCode, IntPtr wp, IntPtr lp)
+            {
+                if (nCode >= 0)
+                {
+                    KBDLLHOOKSTRUCT objKeyInfo = (KBDLLHOOKSTRUCT)Marshal.PtrToStructure(lp, typeof(KBDLLHOOKSTRUCT));
+
+                    // Disabling Windows keys 
+
+                    if (objKeyInfo.key == Keys.RWin || objKeyInfo.key == Keys.LWin || objKeyInfo.key == Keys.Tab && HasAltModifier(objKeyInfo.flags) || objKeyInfo.key == Keys.F4 && HasAltModifier(objKeyInfo.flags) || objKeyInfo.key == Keys.Escape && HasAltModifier(objKeyInfo.flags) || objKeyInfo.key == Keys.Escape && (ModifierKeys & Keys.Control) == Keys.Control)
+                    {
+                        return (IntPtr)1; // if 0 is returned then All the above keys will be enabled
+                    }
+                }
+                return CallNextHookEx(ptrHook, nCode, wp, lp);
+            }
+
+            bool HasAltModifier(int flags)
+            {
+                return (flags & 0x20) == 0x20;
+            }
+            private void MainForm_Load(object sender, EventArgs e)
+            {
+                //killExplorer(); //GALAU MAU PAKE ATAU ENGGA
+                KeyPreview = true;
+                ProcessModule objCurrentModule = Process.GetCurrentProcess().MainModule;
+                objKeyboardProcess = new LowLevelKeyboardProc(captureKey);
+                ptrHook = SetWindowsHookEx(13, objKeyboardProcess, GetModuleHandle(objCurrentModule.ModuleName), 0);
+            }
+            /* Code to Disable WinKey, Alt+Tab, Ctrl+Esc Ends Here */
+            /***==============================================================================***/
+            /***===================================KILLEXPLORER===============================***/
+            private void killExplorer()
+            {
+                int hwnd;
+                hwnd = FindWindow("Progman", null);
+                PostMessage(hwnd, /*WM_QUIT*/ 0x12, 0, 0);
+                return;
+            }
+            /***===============================================================================***/
+            /***=====================================DISABLE ALT + F4==========================***/
+            private bool _altF4Pressed;
+            private void MainForm_KeyDown(object sender, KeyEventArgs e)
+            {
+                if (e.Alt && e.KeyCode == Keys.F4)
+                {
+                    _altF4Pressed = true;
+                }
+            }
+            private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+            {
+                if (_altF4Pressed)
+                {
+                    if (e.CloseReason == CloseReason.UserClosing)
+                        e.Cancel = true;
+                    _altF4Pressed = false;
+                }
+            }
+            public void StartListening(Label x)
+            {
+                var tcp = new TcpListener(IPAddress.Any, 25565);
+                tcp.Start();
+                x.Text = "Listening ...";
+                var listeningThread = new Thread(() =>
+                {
+                    while (true)
+                    {
+                        var tcpClient = tcp.AcceptTcpClient();
+                        x.Text = "Accepted";
+                        ThreadPool.QueueUserWorkItem(param =>
+                        {
+                            NetworkStream stream = tcpClient.GetStream();
+                            string incomming;
+                            byte[] bytes = new byte[1024];
+                            int i = stream.Read(bytes, 0, bytes.Length);
+                            incomming = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+
+
+                            MessageBox.Show(String.Format("Received: {0}", incomming));
+
+                            tcpClient.Close();
+                        }, null);
+                    }
+                });
+
+                listeningThread.IsBackground = true;
+                listeningThread.Start();
+            }
+            /***===================================================================================***/
+            public MainForm()
+            {
+                InitializeComponent();
+                StartListening(labelStatus);
+            }
+        
+    }
+}
